0013ccc01cefdf71c576c6e510bb778d8ee6f127
Tyler Smith <tylerlwsmith@gmail.com>
2024-12-27 08:42:14 -0800

    Fix footer text spacing

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,12 @@
-function currentYear() {
-  return new Date().toLocaleDateString("en-us", {
-    year: "numeric",
-    timeZone: "America/Los_Angeles",
-  });
-}
-
 export function Footer() {
   return (
     <footer class="bg-gray-900 px-10 py-4 text-gray-300 text-center text-md flex justify-between">
       <div>
-        ©
+        ©{" "}
         <a href="https://deadhandmedia.com/" class="text-pink-500 underline">
           Tyler Smith
-        </a>
-        {" " + currentYear()}
+        </a>{" "}
+        {new Date().getFullYear()}
       </div>
       <div>
         <a href="/privacy-policy" class="text-pink-500">
